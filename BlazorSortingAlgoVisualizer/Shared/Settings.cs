d7e0caef76cb9560447c3f7d703092e54b2dd190
harryprs <harryprs@hotmail.co.uk>
2023-06-22 21:40:29 +0100

    Added a Settings class to control min and max values

---
+++ BlazorSortingAlgoVisualizer/Shared/Settings.cs
@@ -0,0 +1,28 @@
+﻿namespace BlazorSortingAlgoVisualizer.Shared
+{
+	public class Settings
+	{
+		private int arraySize;
+		public int ArraySize
+		{
+			get => arraySize;
+			set => arraySize = value < 2 ? 2 : value > 600 ? 600 : value;
+		}
+
+		private int delay;
+		public int Delay
+		{
+			get => delay;
+			set => delay = value < 1 ? 1 : value > 100 ? 100 : value;
+		}
+
+		public bool IsDisabled { get; set; }
+
+		public Settings(int arraySize, int delay, bool isDisabled) 
+		{
+			this.arraySize = arraySize;
+			this.delay = delay;
+			this.IsDisabled = isDisabled;
+		}
+	}
+}
